5cde058cb7abdec75e9480ecebab454afa54cf3c
Johan <angeljohan167@gmail.com>
2023-09-25 18:33:46 -0500

    Se cambio Principal__

--- src/main/java/ejemplo/Principal.java
+++ src/main/java/ejemplo/Principal.java
@@ -3,8 +3,8 @@ package ejemplo;
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("Hola a todos :)");
-
+		System.out.println("Hola mundo");
+		System.out.println("Buenas noches");
 	}
 
 }
